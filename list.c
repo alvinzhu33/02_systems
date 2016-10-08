542d28d5ee33c28fcc4ba4e5d5320b9b0833d334
Alvin Zhu <alvinzhu33@yahoo.com>
2016-10-08 14:01:00 -0400

    Progess on print and insert

--- list.c
+++ list.c
@@ -4,12 +4,18 @@
 struct node { int i; struct node *next };
 
 void print_list ( struct node *list ){
-    printf("[ %s ", *list);
+    printf("[ %s ", list.i);
     while(list.next != 0 ){
-        *list ++;
-        printf(", %s ", *list);
+        printf(", %s ", list.next);
     }
     printf("]");
+}
+
+struct node * insert_front (struct node *list, int data){
+    struct node *new = malloc(sizeof(struct node));
+    new.i = data;
+    new.next = list;
+    return new;
 }
 
 int main(){
